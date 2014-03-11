e2c39f4aad7d3a4635f6e53ba8b869fadd5767af
Nathan Lowrie <nate@finelineautomation.com>
2014-03-11 01:25:05 -0400

    Temporary commit for handling save and continue integration until a proper post hook is put in place.
    https://github.com/spree-contrib/better_spree_paypal_express/issues/32 has the details.
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

---
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -0,0 +1,16 @@
+module Spree
+  CheckoutController.class_eval do
+    before_filter :redirect_to_paypal_express_form_if_needed, :only => [:update]
+
+    private
+    def redirect_to_paypal_express_form_if_needed
+      binding.pry
+      return unless (params[:state] == "payment") && params[:order][:payments_attributes]
+
+      payment_method = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
+      if payment_method.kind_of?(Spree::Gateway::PayPalExpress)
+        redirect_to paypal_express_url(:payment_method_id => payment_method.id)
+      end
+    end
+  end
+end
